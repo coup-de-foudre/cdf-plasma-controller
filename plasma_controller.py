bb4875eb4a7917c9e9485ad39bdf8a06d93814bb
Michael McCoy <michael.b.mccoy@gmail.com>
2018-05-12 17:46:09 -0700

    Refactored controller to better architecture

--- plasma_controller.py
+++ plasma_controller.py
@@ -22,22 +22,32 @@
 
 import argparse
 import sys
+from typing import List
 
-from controller.keyboard_controller import KeyboardController
+from controller.keyboard_controller import KeyboardController, \
+    ProportionalTickKnob, SimpleKnob, AbstractKeyboardKnob
+from interrupter.base_interrupter import BaseInterrupter
+from modulator.base_modulator import BaseModulator
+from modulator.callback_modulator import CallbackModulator
 from pwm.mock_pwm import MockPWM
 from pwm.pi_pwm import PiHardwarePWM
 from interrupter.simple_interrupter import SimpleInterrupter
 
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Run the CdF Plasma Controller with a keyboard controller")
+
+    parser.add_argument(
+        "--mock",
+        action="store_true",
+        help="use mock PWM to test controller without a Pi")
 
     parser.add_argument(
         "--host",
         type=str,
         default=None,
-        help="raspberry PI host (None=local daemon)",
+        help="RPi host (None=local daemon)",
     )
 
     parser.add_argument(
@@ -49,19 +59,27 @@ def main():
     )
 
     parser.add_argument(
+        '-m', '--modulator-frequency',
+        dest='modulator_frequency',
+        type=float,
+        default=0.0,
+        help="frequency (Hz) of FM modulator",
+    )
+
+    parser.add_argument(
+        '-M', '--modulator-spread',
+        dest='modulator_spread',
+        type=float,
+        default=0.0,
+        help="frequency spread (Hz) of FM modulator",
+    )
+
+    parser.add_argument(
         '-D',
         dest='interrupter_duty_cycle',
         type=float,
         default=1.0,
         help="duty cycle of the interrupter (default == 1.0 == no interrupter)",
-    )
-
-    parser.add_argument(
-        '-d',
-        dest='pwm_duty_cycle',
-        type=float,
-        default=0.5,
-        help="duty cycle of the PWM (0.5)",
     )
 
     parser.add_argument(
@@ -73,6 +91,14 @@ def main():
     )
 
     parser.add_argument(
+        '-d',
+        dest='pwm_duty_cycle',
+        type=float,
+        default=0.5,
+        help="duty cycle of the PWM (0.5)",
+    )
+
+    parser.add_argument(
         '-f',
         dest='pwm_frequency',
         type=float,
@@ -80,11 +106,93 @@ def main():
         required=True
     )
 
-    parser.add_argument("--mock",
-                        action="store_true",
-                        help="use mock PWM to test controller")
-
     args = parser.parse_args()
+
+    return args
+
+
+def keyboard_control_knobs(interrupter: BaseInterrupter,
+                           pwm_frequency_modulator: BaseModulator,
+                           ) -> List[AbstractKeyboardKnob]:
+
+    pwm = interrupter.pwm
+
+    interrupter_frequency_knob = ProportionalTickKnob(
+        "Interrupter frequency (Hz)",
+        lambda f: interrupter.set_frequency(f),
+        "←",
+        "→",
+        0.0,
+        float("inf"),
+        interrupter.frequency,
+    )
+
+    interrupter_duty_cycle_knob = SimpleKnob(
+        "Interrupter duty cycle",
+        lambda d: interrupter.set_duty_cycle(d),
+        '{',
+        '}',
+        0.0,
+        1.0,
+        interrupter.duty_cycle,
+    )
+
+    pwm_frequency_knob = ProportionalTickKnob(
+        "PWM frequency (Hz)",
+        lambda c: pwm_frequency_modulator.set_center(c),
+        "↓",
+        "↑",
+        0.0,
+        float("inf"),
+        pwm_frequency_modulator.center,
+    )
+
+    pwm_duty_cycle_knob = SimpleKnob(
+        "PWM duty cycle",
+        lambda d: pwm.set_duty_cycle(d),
+        '<',
+        '>',
+        0.0,
+        1.0,
+        pwm.duty_cycle,
+    )
+
+    pwm_frequency_modulation_freq_knob = SimpleKnob(
+        "PWM FM frequency (Hz)",
+        lambda f: pwm_frequency_modulator.set_frequency(f),
+        "_",
+        "+",
+        min_value=0.0,
+        max_value=30.0,
+        initial_value=pwm_frequency_modulator.frequency,
+        num_ticks=30,
+    )
+
+    pwm_frequency_modulation_intensity_knob = ProportionalTickKnob(
+        "PWM FM spread (Hz)",
+        lambda i: pwm_frequency_modulator.set_spread(i),
+        "(",
+        ")",
+        min_value=0.0,
+        max_value=float("inf"),
+        initial_value=pwm_frequency_modulator.spread,
+    )
+
+    knobs = [
+        interrupter_frequency_knob,
+        pwm_frequency_knob,
+        interrupter_duty_cycle_knob,
+        pwm_duty_cycle_knob,
+        pwm_frequency_modulation_freq_knob,
+        pwm_frequency_modulation_intensity_knob,
+    ]
+
+    return knobs
+
+
+def main():
+
+    args = parse_arguments()
 
     if args.mock:
         pwm = MockPWM()
@@ -98,15 +206,25 @@ def main():
                                     args.interrupter_frequency,
                                     args.interrupter_duty_cycle)
 
-    controller = KeyboardController(interrupter)
+    pwm_frequency_modulator = CallbackModulator(
+        lambda f: pwm.set_frequency(f),
+        frequency=args.modulator_frequency,
+        spread=args.modulator_spread,
+        center=pwm.frequency,
+    )
+
+    control_knobs = keyboard_control_knobs(interrupter, pwm_frequency_modulator)
+    controller = KeyboardController(control_knobs)
 
     # Try-catch-finally is a workaround for issue here:
     # https://www.raspberrypi.org/forums/viewtopic.php?t=66445&start=175#p1156097
     try:
+        interrupter.start()
         controller.run()
     except AttributeError:
         pass
     finally:
+        interrupter.stop()
         pwm.stop()
 
 
